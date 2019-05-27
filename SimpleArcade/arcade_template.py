d733247dc90ae2a902788a2bb5900e7ec992fc67
jadenhan6 <seongmin.han21@ycdsbk12.ca>
2019-05-27 16:55:36 -0400

    Fixed issue in template file that prohibited any form of animation

--- SimpleArcade/arcade_template.py
+++ SimpleArcade/arcade_template.py
@@ -47,6 +47,7 @@ def setup():
     # Override arcade window methods; uses our OWN functions
     window = arcade.get_window()
     window.on_draw = on_draw
+    window.on_update = on_update
     window.on_key_press = on_key_press
     window.on_key_release = on_key_release
     window.on_mouse_press = on_mouse_press
